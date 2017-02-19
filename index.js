daedd45191e84407737255fc05989fdc4ed0d9cf
mythfish <mythfish@gmail.com>
2017-02-20 00:14:16 +0800

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNRigoFingerprint } = NativeModules;
+
+export default RNRigoFingerprint;
---
+++ ios/RNRigoFingerprint.h
@@ -0,0 +1,7 @@
+
+#import "RCTBridgeModule.h"
+
+@interface RNRigoFingerprint : NSObject <RCTBridgeModule>
+
+@end
+  
